7770541435cf1355897a59ccfd2f0fe03f16129c
dnzgnrd <gnrdnz03@gmail.com>
2023-10-10 17:19:09 +0300

    refactor

--- lists.py
+++ lists.py
@@ -1,3 +1,27 @@
+# diziler     #for ile daha kolay yapılabilir
+meyveler = ["Erik", "elma", "kiraz", "kavun"]
+
+print(meyveler)
+
+print(meyveler[0])
+print(meyveler[1])
+print(meyveler[2])
+print(meyveler[3])
+print(len(meyveler))
+
+meyveler[2] = "cilek"
+print(meyveler)
+
+sayilar = [34, 90, 32, 89, 100]
+
+enBuyuk = sayilar[0]
+for sayi in sayilar:
+
+    if sayi > enBuyuk:
+        enBuyuk = sayi
+
+print("en büyük sayı", enBuyuk)
+
 #append ve remove fonk listeye yeni eleman eklemek ve var olan elamı silmek için kullanılır
 sayilar = [1,2,3]
 sayilar.append(5)
